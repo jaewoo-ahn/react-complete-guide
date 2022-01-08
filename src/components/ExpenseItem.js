9996f634ed667a7126e8bc2051665cf613655e9f
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-01-08 16:42:28 +0900

    learning props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   const expenseDate = new Date(2021, 2, 25);
   const expenseTitle = "Car insurance";
   const expensePrice = 294.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">${expensePrice}</div>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }
